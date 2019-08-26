266951dae062d847139fb07dd86971068e04bdb2
Kristina Cayetano <kma.cayetano@gmail.com>
2019-08-25 20:54:20 -0400

    write file stuff

--- app.js
+++ app.js
@@ -2,36 +2,79 @@ const http = require('http')
 const fs = require('fs')
 
 const readFile = (file) => {
-    return new Promise ((resolve,reject)=>{
-        fs.readFile(file,(err,data)=>{
-            if(err){
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) {
                 reject(err)
             }
-            else{
+            else {
                 resolve(data.toString())
             }
         })
     })
 }
-http.createServer((req,res)=>{
-    if(req.url === '/api/guests'){
-        readFile('./guests.json')
-        .then( data => {
-            res.write(data)
-            res.end()
+
+const writeFile = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, (err) => {
+            if (err) {
+                reject(err)
+            }
+            else {
+                resolve()
+            }
         })
-        .catch( ex => {
-            res.write(ex.message)
+    })
+}
+
+const addUser = (user) => {
+    return readFile('./guests.json')
+        .then(data => {
+            const users = JSON.parse(data)
+            users.push(user)
+            return writeFile('./guests.json', JSON.stringify(users))
         })
+}
+
+http.createServer((req, res) => {
+    if (req.url === '/api/guests') {
+        if (req.method === 'GET') {
+            readFile('./guests.json')
+                .then(data => {
+                    res.write(data)
+                    res.end()
+                })
+                .catch(ex => {
+                    res.write(ex.message)
+                    res.end()
+                })
+        } else if (req.method === 'POST') {
+            let user = ''
+            req.on('data', (chunk) => {
+                user += chunk
+            })
+            req.on('end', () => {
+                addUser(user)
+                    .then(() => {
+                        res.write(user)
+                        res.end()
+                    })
+                    .catch(ex => {
+                        res.write(ex.message)
+                        res.end()
+                    })
+            })
+        }
     }
-    if(req.url === '/'){
+    else if (req.url === '/') {
         readFile('./index.html')
-        .then( data => {
-            res.write(data)
-            res.end()
-        })
-        .catch( ex => {
-            res.write(ex.message)
-        })
+            .then(data => {
+                res.write(data)
+                res.end()
+            })
+            .catch(ex => {
+                res.write(ex.message)
+                res.end()
+            })
     }
 }).listen(4200)
